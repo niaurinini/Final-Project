860e56b8539ea8b32d2f806a54675bcf29820215
niaurinini <niaurinino4@gmail.com>
2024-07-10 11:35:52 +0400

    add footer

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+//
+let navigation = document.getElementById("navigationBar");
+let burger = document.getElementById("burgerBar");
+
+burger.addEventListener("click", function() {
+    navigation.classList.toggle("newNavigation")
+})
+
+let button = document.getElementById("clickbutton");
+
+btn.addEventListener("click", function() {
+    alert("sent")
+})
+
